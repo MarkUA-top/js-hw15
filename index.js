60954a52305815ce35b508fcb1853964b0ed5499
MarkUA <mark.d.nahorniak@gmail.com>
2025-01-06 19:37:12 +0200

    15

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 const users = [
-    {name: "User1", balance: 100, friends: ["Tima", "Dima"], skills: ["JavaScript", "HTML", "CSS"]},
+    {name: "User1", balance: 100, friends: ["Tima", "Dima", "Kiril"], skills: ["JavaScript", "HTML", "CSS"]},
     {name: "User2", balance: 200, friends: ["Artem"], skills: ["Python", "Java", "CSS"]},
     {name: "User3", balance: 150, friends: ["Tima", "Artem"], skills: ["HTML", "JavaScript", "Node.js"]}
   ];
   
   // Task 1
-  let totalBalance = 0;
-  users.forEach(user => {
-    totalBalance += user.balance;
-  });
-  console.log(totalBalance);
+  const totalBalance = users.reduce((totalBalance, num) => totalBalance += num.balance, 0)
+  console.log(totalBalance)
   
   // Task 2
-  const nameToFind = "John";
-  let usersWithFriend = [];
-  users.forEach(user => {
-    if (user.friends.includes(nameToFind)) {
-      usersWithFriend.push(user.name);
+  const nameToFind = users.reduce((arrFriend, user) => {
+    if (user.friends.includes("Artem")){
+      arrFriend.push(user.name)
     }
-  });
-  console.log(usersWithFriend);
+    return arrFriend
+  }, [])
+  console.log(nameToFind);
   
   // Task 3
-  const sortedUsers = users
-    .map(user => ({ name: user.name, friendsCount: user.friends.length }))
-    .sort((a, b) => a.friendsCount - b.friendsCount)
-    .map(user => user.name);
-  console.log(sortedUsers);
+  const countFriend = users.reduce((friendCount, user) => {
+    friendCount.push(user.friends.length + " " + user.name)
+    return friendCount
+  }, [])
+  const sortedFriend = countFriend.sort()
+
+  console.log(sortedFriend);
   
   // Task 4
 const allSkills = [];
